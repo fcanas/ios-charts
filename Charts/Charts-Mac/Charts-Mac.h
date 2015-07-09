4c6956d07e701f402fc2d4d4e326addbc4d3b6f6
Fabian Canas <fcanas@gmail.com>
2015-07-09 15:40:12 -0400

    Create Mac framework target

---
+++ Charts/Charts-Mac/Charts-Mac.h
@@ -0,0 +1,19 @@
+//
+//  Charts-Mac.h
+//  Charts-Mac
+//
+//  Created by Fabian Canas on 7/9/15.
+//  Copyright (c) 2015 dcg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Charts-Mac.
+FOUNDATION_EXPORT double Charts_MacVersionNumber;
+
+//! Project version string for Charts-Mac.
+FOUNDATION_EXPORT const unsigned char Charts_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Charts_Mac/PublicHeader.h>
+
+
---
+++ Charts/Classes/Utils/PlatformBridge.swift
@@ -0,0 +1,9 @@
+//
+//  PlatformBridge.swift
+//  Charts
+//
+//  Created by Fabian Canas on 7/9/15.
+//  Copyright (c) 2015 Fabian Canas. All rights reserved.
+//
+
+import Foundation
